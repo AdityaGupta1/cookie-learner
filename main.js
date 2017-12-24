ef2b1882501678455d0dd07e24ef635c91c18661
AdityaGupta1 <adityag@gmail.com>
2017-12-23 20:50:54 -0800

    running multiple networks successfully

--- main.js
+++ main.js
@@ -26,39 +26,38 @@ function getProduct(id) {
 
 // 0 = cookies, 1 = cps, others = product id + 2
 function calculateInput(input) {
-    switch (input) {
+    switch (true) {
         default:
-        case 0:
+        case (input === 0):
             return getSqrtCookies();
             break;
-        case 1:
+        case (input === 1):
             return getCPS();
             break;
-        case 2:
-        case 3:
+        case (input > 1):
             return getProduct(input - 2);
             break;
     }
 }
 
-var network = [[], []];
+var currentNetwork = [[], []];
 
 function fillNetwork(inputNodes, hiddenNodes, outputNodes) {
     for (var i = 0; i < inputNodes; i++) {
-        network[0][i] = new Array(hiddenNodes);
+        currentNetwork[0][i] = new Array(hiddenNodes);
     }
 
     for (var j = 0; j < hiddenNodes; j++) {
-        network[1][j] = new Array(outputNodes);
+        currentNetwork[1][j] = new Array(outputNodes);
     }
 
     // layers of nodes
-    for (var k = 0; k < network.length; k++) {
+    for (var k = 0; k < currentNetwork.length; k++) {
         // individual nodes
-        for (var l = 0; l < network[k].length; l++) {
+        for (var l = 0; l < currentNetwork[k].length; l++) {
             // individual weights
-            for (var m = 0; m < network[k][l].length; m++) {
-                network [k][l][m] = (Math.random() * 50) - 25;
+            for (var m = 0; m < currentNetwork[k][l].length; m++) {
+                currentNetwork [k][l][m] = (Math.random() * 50) - 25;
             }
         }
     }
@@ -67,8 +66,8 @@ function fillNetwork(inputNodes, hiddenNodes, outputNodes) {
 function calculateHidden(hidden) {
     var sum = 0;
 
-    for (var i = 0; i < network[0].length; i++) {
-        sum += calculateInput(i) * network[0][i][hidden];
+    for (var i = 0; i < currentNetwork[0].length; i++) {
+        sum += calculateInput(i) * currentNetwork[0][i][hidden];
     }
 
     return sum;
@@ -78,15 +77,21 @@ function calculateHidden(hidden) {
 function calculateOutput(output) {
     var sum = 0;
 
-    for (var i = 0; i < network[1].length; i++) {
-        sum += calculateHidden(i) * network[1][i][output];
+    for (var i = 0; i < currentNetwork[1].length; i++) {
+        sum += calculateHidden(i) * currentNetwork[1][i][output];
     }
 
     return sum;
 }
 
 function getOutputs() {
-    return [calculateOutput(0), calculateOutput(1), calculateOutput(2)];
+    var outputs = [];
+
+    for (var i = 0; i < currentNetwork[0].length; i++) {
+        outputs[i] = calculateOutput(i);
+    }
+
+    return outputs;
 }
 
 function getIndexOfMax(array) {
@@ -100,7 +105,6 @@ function getIndexOfMax(array) {
         }
     }
 
-    console.log(array);
     return index;
 }
 
@@ -117,35 +121,84 @@ function buyProduct(id) {
     Game.ClickProduct(id);
 }
 
+var numberOfNetworks = 10;
+var networks = new Array(numberOfNetworks);
+
+for (var i = 0; i < numberOfNetworks; i++) {
+    networks[i] = new Array(2);
+}
+
+var networkIndex = 0;
+
+// time in seconds to run each network
+var networkTime = 60;
+networkTime *= 1000;
+networkTime += 100;
+
+
 function doStuff() {
     var option = getIndexOfMax(getOutputs());
 
-    switch (option) {
+    console.log('outputs: ' + getOutputs());
+    console.log('option: ' + option);
+
+    switch (true) {
         default:
-        case 0:
+        case (option === 0):
             clickCookie();
             break;
-        case 1:
-        case 2:
+        case (option > 0):
             buyProduct(option - 1);
             break;
     }
 }
 
-function getFitness(time) {
-    var loop = setInterval(function(){ doStuff(); }, 100);
-    setTimeout(function(){
+function runNetwork(timeToRun) {
+    console.log('network ' + networkIndex);
+
+    var loop = setInterval(function() { doStuff(); }, 100);
+    setTimeout(function() {
         clearInterval(loop);
         var fitness = (getCPS() * 100) + getCookies();
         doStuffWithFitness(fitness);
-    }, time);
+    }, timeToRun);
+}
+
+function createRandomNetworks() {
+    for (var i = 0; i < numberOfNetworks; i++) {
+        currentNetwork = [[], []];
+        fillNetwork(5, 5, 5);
+        networks[i][0] = currentNetwork.slice();
+    }
+
+    currentNetwork = networks[0][0];
 }
 
 function start() {
-    fillNetwork(3, 4, 5);
-    console.log(getFitness(60000));
+    hardReset();
+
+    createRandomNetworks();
+
+    runNetwork(networkTime);
 }
 
 function doStuffWithFitness(fitness) {
-    console.log(fitness);
+    networks[networkIndex][1] = fitness;
+
+    hardReset();
+
+    setTimeout(function() {
+        networkIndex++;
+
+        if (networkIndex === 10) {
+            console.log(networks);
+
+            // move to next generation
+
+            return;
+        }
+
+        currentNetwork = networks[networkIndex][0];
+        runNetwork(networkTime);
+    }, 1000);
 }
